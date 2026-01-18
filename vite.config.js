0c2c391d0dbdf7f4bc501d67c08b8247dd3b1866
Olivier Elamine <johndoe@example.com>
2026-01-18 17:53:06 +0200

    Updated repo name for Vite base path.

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/<REPO_NAME>/', // Replace with your GitHub repo name for Pages.
+  // base: '/<REPO_NAME>/', // Replace with your GitHub repo name for Pages.
+  base: '/Usefull-Calendar/',
   plugins: [react()],
   test: {
     environment: 'jsdom',
